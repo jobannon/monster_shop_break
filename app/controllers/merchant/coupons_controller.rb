82b3495d342b25ec10dacf12981b5a8d9e3934f4
Joshua O'bannon <jtobannon@gmail.com>
2020-01-11 22:49:16 -0700

    Add dleete and show action for the Coupon Controller

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -2,4 +2,13 @@ class Merchant::CouponsController < ApplicationController
   def index
     @merchant_coupons = Coupon.where(merchant_id: current_user.merchant_id) 
   end 
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def destroy
+    Coupon.delete(params[:id])
+    redirect_to merchant_coupons_path
+  end
 end 
