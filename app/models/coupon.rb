8c295c9ce40ed61e6cc5d264a1b9c3c29a09efe6
Joshua O'bannon <jtobannon@gmail.com>
2020-01-12 15:55:42 -0700

    Add validates for Coupon Model

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,5 @@
 class Coupon < ApplicationRecord
   belongs_to :merchant
+  validates_presence_of :percentage_off, :name, :coupon_code 
+  validates_numericality_of :percentage_off, less_than: 100 
 end
