f3c3dfb32d5bfa1e03f811759afe8f1d0f02eafa
Joshua O'bannon <jtobannon@gmail.com>
2020-01-12 15:24:00 -0700

    Add CRUD functionality fo Coupons Controller

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -3,6 +3,14 @@ class Merchant::CouponsController < ApplicationController
     @merchant_coupons = Coupon.where(merchant_id: current_user.merchant_id) 
   end 
 
+  def create 
+    binding.pry
+  end
+
+  def new
+    binding.pry
+  end
+
   def show
     @coupon = Coupon.find(params[:id])
   end
@@ -11,4 +19,20 @@ class Merchant::CouponsController < ApplicationController
     Coupon.delete(params[:id])
     redirect_to merchant_coupons_path
   end
+
+  def edit
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def update
+    coupon = Coupon.find(params[:id])
+    coupon.update!(coupon_params)
+    redirect_to merchant_coupons_path
+  end
+
+  private
+
+    def coupon_params
+      params.permit(:name, :coupon_code, :percentage_off)
+    end
 end 
