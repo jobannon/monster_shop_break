b8889fe7f56e2d16607e0f59674d994cd10a2555
Joshua O'bannon <jtobannon@gmail.com>
2020-01-14 16:11:53 -0700

    Work on delete spec

--- spec/features/coupons/delete_spec.rb
+++ spec/features/coupons/delete_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 RSpec.describe "as a merchant user" do 
   describe "when I visit the the merchant coupons show page" do 
     before(:each) do 
-      @target = Merchant.create!(name: "target", address: "100 some drive", city: "denver", state: "co", zip: 80023)
-      @coupon_1 = Coupon.create!(name: "Summer Saver", coupon_code: "sum-save", percentage_off: 10, merchant_id: @target.id)
-      @coupon_2 = Coupon.create!(name: "Winter Saver", coupon_code: "winter-save", percentage_off: 50, merchant_id: @target.id)
-      @merchant_user = User.create!(name: "show merch", address: "show", city: "denver", state: "co", zip: 80023, role: 2, email: "joe3@ge.com", password: "password")
+      @target = Merchant.create!(name: "target coup", address: "100 some drive", city: "denver", state: "co", zip: 80023)
+      @merchant_user = @target.users.create!(name: "show merch", address: "show", city: "denver", state: "co", zip: 80023, role: 2, email: "joe3@ge.com", password: "password")
 
-      @target.users << @merchant_user 
+      @coupon_1 = @target.coupons.create!(name: "Summer Saver", coupon_code: "sum-save", percentage_off: 10)
+      @coupon_2 = @target.coupons.create!(name: "Winter Saver", coupon_code: "winter-save", percentage_off: 50) 
       
       #log in as a merch
       visit login_path
@@ -27,6 +26,21 @@ RSpec.describe "as a merchant user" do
       end
 
       expect(page).not_to have_content(@coupon_1.name)
+      expect(page).to have_content(@coupon_2.name)
+    end 
+
+    it "will not let me delete the coupon if its already on an order" do 
+      visit merchant_coupon_path(@coupon_1)
+
+      mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      paper = mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
+      pencil = mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+
+      user = User.create!(name: "jess", address: "jess way", city: "denver", state: "co", zip: 80023, role: 0, email: "jess@j.com", password: "password")
+      user.orders.create(name: "bob", address: "1 way", city: "denver", state: "md", zip: 22235)
+      holder = user.orders[0].item_orders.create!({item: paper, quantity: 1, price: 10 })
+      binding.pry
+      
     end 
   end
 end 
